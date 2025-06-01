1276ea496eb0369fc879d62ea300eec4d04db868
0CrazyLove <axelvasquez582@gmail.com>
2025-06-01 13:55:37 -0500

    ...

--- MiPrimerMVC/Controllers/HomeController.cs
+++ MiPrimerMVC/Controllers/HomeController.cs
@@ -6,11 +6,27 @@ namespace MiPrimerMVC.Controllers;
 
 public class HomeController : Controller
 {
+
     public IActionResult Index()
     {
         ViewData["Mensaje"] = "Crazy se esta viniendo en el backend";
         ViewData["pene"] = "probandoooo";
         ViewData["hola"] = "lol";
         return View();
+    }
+
+    //nuevo metodo
+    public IActionResult Saludo()
+    {
+        ViewData["Mensaje"] = "HOLA! SOY GAY Y ME GUSTA LA POLLA DE MUJERES TRANSSS THERIAN!!";
+        return View();
+
+    }
+
+    //nuevo metodo 2
+    public IActionResult Fecha()
+    {
+        ViewData["hora"] = DateTime.Now;
+        return View();  
     }
 }
