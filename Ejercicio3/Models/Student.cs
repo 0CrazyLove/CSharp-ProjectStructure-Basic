b72a46b512aef6075d803a84b36f7dc4a1bf9262
0CrazyLove <axelvasquez582@gmail.com>
2025-06-07 17:32:07 -0500

    ...

--- Ejercicio3/Models/Student.cs
+++ Ejercicio3/Models/Student.cs
@@ -8,4 +8,44 @@ public class Student
     private string? name;
     private int age;
     private string? course;
+    //constructor
+    public Student()
+    {
+        Id = id;
+        Name = name!;
+
+    }
+
+    //propiedad para id
+    public int Id
+    {
+        get { return id; }
+        set
+        {
+            if (value <= 0)
+            {
+                System.Console.WriteLine("valor no permitido");
+            }
+            else
+            {
+                id = value;
+            }
+        }
+    }
+    //propiedad para name
+    public string Name
+    {
+        get { return name!; }
+        set
+        {
+            if (String.IsNullOrEmpty(value))
+            {
+                System.Console.WriteLine("valor del nombre no permitido");
+            }
+            else
+            {
+                name = value;
+            }
+        }
+    }
 }
