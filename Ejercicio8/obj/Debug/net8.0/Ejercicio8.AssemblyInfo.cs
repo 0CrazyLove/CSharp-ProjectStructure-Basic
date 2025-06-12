8870d8dd09ecb375cab3b119099df001ca73c7f3
0CrazyLove <axelvasquez582@gmail.com>
2025-06-11 21:01:11 -0500

    ...

--- Ejercicio8/obj/Debug/net8.0/Ejercicio8.AssemblyInfo.cs
+++ Ejercicio8/obj/Debug/net8.0/Ejercicio8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76c275ac508406fb20390c32f4407727c80c2a0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1d7b8f193acac21b8fe845fc2463742b6a1dac3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejercicio8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
