0558ac45da10152218a222e4be0fe1274dd3061d
Crazy <axelvasquez582@gmail.com>
2025-06-30 20:45:02 -0500

    ...

---
+++ Ejercicio15/WebApplication1/WebApplication1/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using WebApplication1.Models;
+using Microsoft.EntityFrameworkCore;
+namespace WebApplication1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
